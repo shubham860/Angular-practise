b6358718ab783f31c6d12730bcbb88dfa3eeae75
@shubham860 <shubham.chauhan860@gmail.com>
2018-07-31 03:49:02 +0530

    update-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 var app = angular.module("todo",[]);
 app.controller("ctrl",function($scope){
-     $scope.item=["milk","mook"];
+     $scope.item = [];
+     $scope.additem = function(){
+       $scope.errortext = "";
+           if (!$scope.newitem) {
+           return;}
+           if ($scope.item.indexOf($scope.newitem) == -1) {
+               $scope.item.push($scope.newitem);
+               $scope.newitem="";
+           } else {
+               $scope.errortext = "The item is already in your shopping list.";
+           }
+       }
+     $scope.removeitem = function(x){
+        $scope.errortext = "";
+         $scope.item.splice(x,1);
+     }
 });
