569230357eba04134514b26981b5a6221f36d8de
@shubham860 <shubham.chauhan860@gmail.com>
2018-08-02 00:57:32 +0530

    update-11

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,19 @@ var app = angular.module("todo",[]);
 app.controller("ctrl",function($scope, $interval){
      $scope.item = [];
      $scope.additem = function(){
-       $scope.errortext = "";
            if (!$scope.newitem) {
-           return;}
+               alert("WARNING : You must enter some task");
+                 return;
+            }
            if ($scope.item.indexOf($scope.newitem) == -1) {
                $scope.item.push($scope.newitem);
                $scope.newitem="";
            } else {
-               $scope.errortext = "WARNING : This item is already exist.";
+               alert("WARNING : This item is already exist.");
+                $scope.newitem="";
            }
        }
      $scope.removeitem = function(x){
-        $scope.errortext = "";
          $scope.item.splice(x,1);
      }
 
@@ -34,3 +35,26 @@ app.controller("ctrl",function($scope, $interval){
       }, 1000);
 
 });
+
+
+$("#ip1").keypress(function(e){
+   var key = e.which;
+   if (key === 13) {
+      additem();
+  }
+ });
+
+ function day() {
+     var d = new Date();
+     var weekday = new Array(7);
+     weekday[0] = "Sunday";
+     weekday[1] = "Monday";
+     weekday[2] = "Tuesday";
+     weekday[3] = "Wednesday";
+     weekday[4] = "Thursday";
+     weekday[5] = "Friday";
+     weekday[6] = "Saturday";
+
+     var n = weekday[d.getDay()];
+     document.getElementById("show").innerHTML = n;
+ }
